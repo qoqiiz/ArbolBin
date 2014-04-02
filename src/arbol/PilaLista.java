9835644281e69a8982c09275abf82222d2a54c56
qoqiiz <Coqiz@Jorge.lan>
2014-04-01 19:59:18 -0600

    segundo comit

--- src/arbol/PilaLista.java
+++ src/arbol/PilaLista.java
@@ -12,4 +12,28 @@ package arbol;
  */
 public class PilaLista {
     
-}
+    Integer [] pila=new Integer [10];
+    int tope=-1;
+    
+    void Push (Integer dato){
+        if(tope <pila.length-1){
+            pila[tope+1]=dato;
+            tope++;
+            
+        }else{
+            System.out.println("Pila Llenaa");
+        }
+    }
+        
+    Integer Pop(){
+        Integer resultado=0;
+        if(tope>-1){
+            resultado=pila[tope];
+        }else
+            System.out.println("Pila Vacia");
+            
+   return resultado;
+        }
+
+        
+    }
